80787767e0d131a48b5152526220f5e8bb68c24c
ImLJS <imljs08@gmail.com>
2023-02-02 10:20:09 +0530

    Second Commit: Movement of Paddle

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title('Pong Game')
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game = True
+while game:
+    screen.update()
+
+screen.exitonclick()
