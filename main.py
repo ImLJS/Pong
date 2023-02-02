0e83d721ab8ca17fcea90dbd09970062632678d3
ImLJS <imljs08@gmail.com>
2023-02-02 12:06:28 +0530

    Seventh Commit: Final Game and Game Over Screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,9 +10,10 @@ screen.bgcolor('black')
 screen.title('Pong Game')
 screen.tracer(0)
 
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -21,18 +23,30 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game = True
 while game:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     if ball.ycor() >= 285 or ball.ycor() <= -285:
-        ball.bounce()
+        ball.bounce_y()
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
-        ball.bounce_paddle()
+        ball.bounce_x()
 
-    if ball.xcor() > 390 or ball.xcor() < -390:
+    if ball.xcor() > 380:
+        ball.reset_position()
+        ball.bounce_x()
+        score.l_score += 1
+        score.update_score()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        ball.bounce_x()
+        score.r_score += 1
+        score.update_score()
+
+    if score.l_score == 3 or score.r_score == 3:
         game = False
-
+        score.game_over()
 
 screen.exitonclick()
