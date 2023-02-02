05384409b8cf1f024d37f6d8a71e4d1cd5123b62
ImLJS <imljs08@gmail.com>
2023-02-02 12:04:53 +0530

    Fifth Commit: Scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.pu()
+        self.hideturtle()
+        self.goto(0,280)
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align='center', font=('Courier', 80, 'normal'))
+        self.goto(100, 200)
+        self.write(self.r_score, align='center', font=('Courier', 80, 'normal'))
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write('Game Over', align='center', font=('Courier', 80, 'normal'))
