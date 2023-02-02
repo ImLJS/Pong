99c123087f52e157c80a0b36ea2cf0c4c35f70ba
ImLJS <imljs08@gmail.com>
2023-02-02 11:04:59 +0530

    Third Commit: Ball Bounce from Walls

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -18,6 +21,12 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game = True
 while game:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    if ball.ycor() >= 290 or ball.ycor() <= -290:
+        ball.bounce()
+
 
 screen.exitonclick()
